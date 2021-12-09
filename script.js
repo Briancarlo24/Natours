c07fba53690304a01a5f1da49deba7c399b10203
Briancarlo24 <Briancarlo2494@gmail.com>
2021-12-09 12:09:20 +0800

    minor js update

--- script.js
+++ script.js
@@ -30,12 +30,9 @@ document
     e.preventDefault();
     navBg.classList.toggle("nav__bg-display");
     navNav.classList.toggle("nav__display");
-    // document
-    //   .querySelector(".navigation__nav")
-    //   .classList.toggle("nav__bg-display nav__display");
+
     // Matching strategy
     if (e.target.classList.contains("navigation__link")) {
-      // console.log('contains nav__link');
       const id = e.target.getAttribute("href"); // returns #section--1/2/3
       const myClass = e.target.getAttribute("class");
       document.querySelector(id).scrollIntoView({ behavior: "smooth" });
